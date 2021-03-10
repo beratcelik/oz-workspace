502cde1d9ccf05a408cb01c8ec6774631ce49f85
Berat Çelik <beratcelik@gmail.com>
2021-03-10 19:57:57 +0300

    Initial commit

---
+++ truffle.js
@@ -0,0 +1,22 @@
+// npm install -g truffle@0.6.0
+// truffle init
+// truffle compile
+// truffle migrate
+
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.7.6",
+    }
+  }
+};
